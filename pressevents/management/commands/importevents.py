75c3f93fdb065f21ed6863735e86c6346a045b12
capooti <pcorti@gmail.com>
2013-10-08 12:38:25 +0200

    Now GDELT database is imported daily.

--- pressevents/management/commands/importevents.py
+++ pressevents/management/commands/importevents.py
@@ -1,6 +1,10 @@
 # imports python
 import csv
+import sys
 import datetime
+import urllib2
+from zipfile import ZipFile
+from StringIO import StringIO
 # imports django
 from django.core.management.base import BaseCommand, CommandError
 from django.conf import settings
@@ -18,16 +22,25 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         #Event.objects.all().delete()
-        keywords = ['earthquake', 'flood', 'rebel', 'refugee']
-        with open('temp/20130929.export.CSV', 'rb') as csvfile:
+        yesterday = datetime.date.today()-datetime.timedelta(1)
+        sdate = yesterday.strftime('%Y%m%d')
+        url = urllib2.urlopen('http://gdelt.utdallas.edu/data/dailyupdates/%s.export.CSV.zip' % sdate)
+        zipfile = ZipFile(StringIO(url.read()))
+        with zipfile.open(zipfile.filelist[0].filename) as csvfile:
+        #with open('temp/20130929.export.CSV', 'rb') as csvfile:
             eventsreader = csv.reader(csvfile, delimiter='\t')
             for row in eventsreader:
                 print row
+                globaleventid = row[0]
+                # if there is the same globaleventid we already imported the csv file!
+                if eventsreader.line_num == 1:
+                    if Event.objects.filter(globaleventid=globaleventid).exists():
+                        print 'This file was already imported!'
+                        sys.exit()
                 lon = row[54]
                 lat = row[53]
                 # we import it only if we have the coordinates
                 if(len(lon)>0 and len(lat)>0):
-                    globaleventid = row[0]
                     date = row[1]
                     url = row[57]
                     num_mentions = row[31]
@@ -57,3 +70,6 @@ class Command(BaseCommand):
                     #import ipdb;ipdb.set_trace()
                     event.geometry = Point(float(lon), float(lat))
                     event.save()
+        self.stdout.write('\n***Removing old events')
+        firstday = datetime.date.today()-datetime.timedelta(days=int(settings.DAYS_TO_KEEP))
+        Event.objects.filter(date__lte=firstday).delete()
